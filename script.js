c69525951b8230ef6fe1466ea3732d1920ed50aa
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-11-08 18:01:55 -0500

    replicating the same for numbers

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // Character generator functions
 
 
-
-function geTRamdomLowerCase (){
-    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-}
-
 // Reference source https://www.w3schools.com/html/html_charset.asp
 
-console.log(geTRamdomLowerCase());
+// LowerCase random function using browser charset "a" being "97" and 25 alphabets to follow
 
+function getRamdomLowerCase () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRamdomLowerCase());
+
+// Replicating the same as in lowerCase random function using browser charset ..with change "A" being 65 and 25 to follow
+
+function getRamdomUpperCase (){
+    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
+console.log(getRamdomUpperCase());
+
+// Replicating thr same as for alphabets function using browser charset ..with change "0" being 48 and 9 to follow
+
+function getRamdomNumber (){
+    return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+}
+
+console.log(getRamdomNumber());
