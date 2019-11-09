9e285a999aa3ef64cfdca08653dfd5de627d5047
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-11-08 20:33:30 -0500

    corrections to naming conventions

--- script.js
+++ script.js
@@ -3,19 +3,19 @@
 
 const outputEl = document.getElementById('password');
 const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numberEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
+const upperEl = document.getElementById('lower');
+const lowerEl = document.getElementById('upper');
+const numberEl = document.getElementById('number');
+const symbolEl = document.getElementById('symbol');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
 
 const randomFunc = {
-    lowercase:  getRamdomLowerCase,
-    uppercase:  getRamdomUpperCase,
-    numbers: getRamdomNumber,
-    symbols: getRamdomsymbol
+    lower:  getRamdomLower,
+    upper:  getRamdomUpper,
+    number: getRamdomNumber,
+    symbol: getRamdomsymbol
 };
 
 // Reference source https://www.w3schools.com/js/js_htmldom_eventlistener.asp
@@ -23,27 +23,32 @@ const randomFunc = {
 // create event listner
 
 generateEl.addEventListener("click", ()  => {
-    const length = parseInt(lengthEl.value);
-    const containsUpperCase = uppercaseEl.checked;
-    const containsLowerCase = lowercaseEl.checked;
+    const length = parseInt(lengthEl.value);    
+    const containsLower = lowerEl.checked;
+    const containsUpper = upperEl.checked;
     const containsNumber = numberEl.checked;
-    const containssymbol = symbolsEl.checked;
+    const containsSymbol = symbolEl.checked;
 
 
     
-    outputEl.innerText = generatePassword(containsLowerCase, 
-        containsNumber, containsUpperCase, containssymbol, length );
+    outputEl.innerText = generatePassword(containsLower, 
+        containsNumber, containsUpper, containsSymbol, length );
 
 });
  
 
 // Generate Passowrd function
-function generatePassword(lowerCase, uppercase, numbers, symbols, length){
+
+function generatePassword(lower, upper, number, symbol, length){
 //initialise passowrd variable
 // remove the uncheked boxes
 // loop over the length and call generator function for each type
 // add final password to the password variable
+ var generatedPassword = "";
 
+ const typesCount = lower + upper + number + symbol;
+ console.log("typescount:", typesCount);
+ 
 
 
 
@@ -56,17 +61,17 @@ function generatePassword(lowerCase, uppercase, numbers, symbols, length){
 
 // LowerCase random function using browser charset "a" being "97" and 25 alphabets to follow
 
-function getRamdomLowerCase () {
+function getRamdomLower () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(getRamdomLowerCase());
+console.log(getRamdomLower());
 
 // Replicating the same as in lowerCase random function using browser charset ..with change "A" being 65 and 25 to follow
 
-function getRamdomUpperCase (){
+function getRamdomUpper (){
     return String.fromCharCode(Math.floor(Math.random()*26) + 65);
 }
-console.log(getRamdomUpperCase());
+console.log(getRamdomUpper());
 
 // Replicating the same as for alphabets function using browser charset ..with change "0" being 48 and 9 to follow
 
@@ -78,9 +83,9 @@ console.log(getRamdomNumber());
 
 // Replicating similar function as above but charCode being avoided for ease of coding
 
-function getRamdomsymbols (){
+function getRamdomsymbol (){
     const symbol = "!@#$%^&*(){}[]<>/?+.";
-    return symbol[Math.floor(Math.random() * symbols.length)];
+    return symbol[Math.floor(Math.random() * symbol.length)];
 }
 
 console.log(getRamdomsymbol());
