f605d7aac8e86eeec55899c47283b1d8f5df3f3d
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-11-12 00:04:59 -0500

    js trimming

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
 
 // DOM elements 
 
-const outputEl = document.getElementById('password');
-const lengthEl = document.getElementById('length');
-const upperEl = document.getElementById('lower');
-const lowerEl = document.getElementById('upper');
-const numberEl = document.getElementById('number');
-const specialCharEl = document.getElementById('specialChar');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
+var outputEl = document.getElementById('password');
+var lengthEl = document.getElementById('length');
+var upperEl = document.getElementById('lower');
+var lowerEl = document.getElementById('upper');
+var numberEl = document.getElementById('number');
+var specialCharEl = document.getElementById('specialChar');
+var generateEl = document.getElementById('generate');
+var clipboardEl = document.getElementById('clipboard');
+var toast = document.getElementById("snackbar");
 
-
-const randomFunc = {
+var randomFunc = {
     lower:  getRamdomLower,
     upper:  getRamdomUpper,
     number: getRamdomNumber,
     specialChar: getRamdomspecialChar
 };
 
-// Reference source https://www.w3schools.com/js/js_htmldom_eventlistener.asp
-
 // create event listner
 
-/*
-
-if (!lowerEl.checked && !upperEl.checked.checked && !numberEl.checked && specialCharEl.checked) {
-    errorMessage.innerHTML = "Well, passwords cannot be blank."
-    return;
-}
-
-*/
-
-
 generateEl.addEventListener("click", function() {
-    const length = parseInt(lengthEl.value);    
-    const containsLower = lowerEl.checked;
-    const containsUpper = upperEl.checked;
-    const containsNumber = numberEl.checked;
-    const containsspecialChar = specialCharEl.checked;
+    var length = parseInt(lengthEl.value);    
+    var containsLower = lowerEl.checked;
+    var containsUpper = upperEl.checked;
+    var containsNumber = numberEl.checked;
+    var containsspecialChar = specialCharEl.checked;
 
-
-    
     outputEl.innerText = generatePassword(containsLower, 
         containsNumber, containsUpper, containsspecialChar, length );
-
-});
- 
-
+}); 
 // Generate Passowrd function
 
 function generatePassword(lower, upper, number, specialChar, length){
-//initialise passowrd variable
-// remove the uncheked boxes
+
+//initialise passowrd variable ;
 // loop over the length and call generator function for each type
 // add final password to the password variable
  
@@ -59,99 +42,65 @@ var generatedPassword = "";
 
 // count number of checked boxes
 
- const typesCount = lower + upper + number + specialChar;
+ var typesCount = lower + upper + number + specialChar;
 
  console.log("typescount:", typesCount);
 
  //array of objects lower, upper, number, specialChar  as key & filter out unckecked
 
- const typesArr = [{lower}, {upper}, {number}, {specialChar}].filter(item  => Object.values(item)[0])
+ var typesArr = [{lower}, {upper}, {number}, {specialChar}].filter(item  => Object.values(item)[0])
 
- console.log("typesArr: ", typesArr);
-
-if (typesCount ===0) {
-     return("!!! Cannot generate bank password !!!"); 
-   
-    }
-
-    if(length > 128) {
+ if (typesCount ===0) {
+     return("!!! Cannot generate blank password !!!");    
+    } if(length > 128) {
         alert("Enter number between 8 and 128");
         return '';        
-    }
-
-    if(length < 8) {
+    }if(length < 8) {
         alert("Enter number between 8 and 128");
-        return '';
-     
-    }        
-    
-
-    for (let i = 0; i < length; i += typesCount) {
+        return '';    
+    }for (let i = 0; i < length; i += typesCount) {
 typesArr.forEach(type =>{
-const functname = Object.keys(type)[0];
-
-    
+var functname = Object.keys(type)[0]; 
 
     generatedPassword += randomFunc[functname]();
 });
-
     }
-  const combinedCharacters =  generatedPassword.slice(0, length);
-
+  var combinedCharacters =  generatedPassword.slice(0, length);
     return combinedCharacters;
-
 }
 
-// copy the password to clipbord
+// Copy the password to clipbord
 
 clipboardEl.addEventListener("click", function() {
-    const textarea = document.createElement('textarea');
-    const password = outputEl.innerText;
+    var textarea = document.createElement('textarea');
+    var password = outputEl.innerText;
 
-    if(!password) {
-        return;
-    }
+    if(!password) {        
+        return "";
+        }
 
     textarea.value = password;
     document.body.appendChild(textarea);
     textarea.select();
     document.execCommand("copy");
-    textarea.remove();
+    textarea.remove();    
 })
 
-
-
 // Character generator functions
-// Reference source https://www.w3schools.com/html/html_charset.asp
-
-// LowerCase random function using browser charset "a" being "97" and 25 alphabets to follow
 
 function getRamdomLower () {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(getRamdomLower());
-
-// Replicating the same as in lowerCase random function using browser charset ..with change "A" being 65 and 25 to follow
-
 function getRamdomUpper (){
     return String.fromCharCode(Math.floor(Math.random()*26) + 65);
 }
-console.log(getRamdomUpper());
-
-// Replicating the same as for alphabets function using browser charset ..with change "0" being 48 and 9 to follow
-
 function getRamdomNumber (){
     return String.fromCharCode(Math.floor(Math.random()*10) + 48);
 }
-
-console.log(getRamdomNumber());
-
-// Replicating similar function as above but charCode being avoided for ease of coding
-
 function getRamdomspecialChar (){
     const specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
     return specialChar[Math.floor(Math.random() * specialChar.length)];
 }
-console.log(getRamdomspecialChar());
+
 
 
